c44bf23e25d27371f76ca46626d0bc9295f3e767
Borriiiissss <chega123@mail.ru>
2022-12-04 18:44:17 +0200

    notebook base

---
+++ BaseNotebook.java
@@ -0,0 +1,13 @@
+public abstract class BaseNotebook {
+    int ramSize;
+    int driveSize;
+    String osType;
+    String color;
+    public BaseNotebook(int ramSize, int driveSize, String osType, String color) {
+        this.ramSize = ramSize;
+        this.driveSize = driveSize;
+        this.osType = osType;
+        this.color = color;
+    }
+
+}
